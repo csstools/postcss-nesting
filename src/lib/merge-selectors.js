21c59fffbeab29d8bbeb5f7d0af7804799b63b51
romainmenke <romainmenke@gmail.com>
2021-10-30 10:09:58 +0200

    fix spec issues

--- src/lib/merge-selectors.js
+++ src/lib/merge-selectors.js
@@ -1,14 +1,20 @@
-import { complex, replaceable } from './valid-selector.js'
+import { replaceable } from './valid-selector.js'
 
 export default function mergeSelectors(fromSelectors, toSelectors) {
-	return fromSelectors.reduce(
-		(selectors, fromSelector) => selectors.concat(
-			toSelectors.map(
-				(toSelector) => complex.test(toSelector)
-					? toSelector.replace(replaceable, `:is(${fromSelector})`)
-				: toSelector.replace(replaceable, fromSelector)
-			)
-		),
-		[]
-	)
+	return toSelectors.map((toSelector) => {
+		let needsIsOnFromSelector = false
+
+		if (fromSelectors.length > 1) {
+			needsIsOnFromSelector = true
+		}
+
+		// foo &foo foo & baz -> foo &:is(foo) foo & baz
+		toSelector = toSelector.replaceAll(/&((?:[\w-_|])(?:[^\s,{]*))/g, (match, p1) => {
+			return `&:is(${p1})`
+		})
+
+		return needsIsOnFromSelector
+				? toSelector.replace(replaceable, `:is(${fromSelectors.join(', ')})`)
+				: toSelector.replace(replaceable, fromSelectors.join(', '))
+	})
 }
