e5f3ee2bf96ecdaedc6e926cfdd400c247e5e83a
romainmenke <romainmenke@gmail.com>
2021-10-30 13:58:47 +0200

    update CI

--- src/lib/merge-selectors.js
+++ src/lib/merge-selectors.js
@@ -9,13 +9,13 @@ export default function mergeSelectors(fromSelectors, toSelectors) {
 		}
 
 		// foo &foo foo & baz -> foo &:is(foo) foo & baz
-		toSelector = toSelector.replaceAll(/&((?:[\w-_|])(?:[^\s,{]*))/g, (match, p1) => {
+		toSelector = toSelector.replace(/&((?:[\w-_|])(?:[^\s,{]*))/g, (match, p1) => {
 			return `&:is(${p1})`
 		})
 
 		// foo& -> foo:is(&)
 		if (fromSelectors.length === 1 && /^(?:[\w-_|])/.test(fromSelectors[0])) {
-			toSelector = toSelector.replaceAll(/([\w-_|]+)(?:&)/g, (match, p1) => {
+			toSelector = toSelector.replace(/([\w-_|]+)(?:&)/g, (match, p1) => {
 				return `${p1}:is(&)`
 			})
 		}
